b8f694726cdef24b36423a5512a73bf89629a974
Trykxx <amitnicolas@gmail.com>
2024-06-24 14:33:22 +0200

    constantes magiques

--- index.php
+++ index.php
@@ -105,7 +105,8 @@
     // Creer une fonction debug, un parametre
     // affiche les balises pre et la variable sera ce que l'on met dans le var-dump
 
-    function debug($props){
+    function debug($props)
+    {
         echo '<pre>';
         var_dump($props);
         echo '</pre>';
@@ -113,9 +114,81 @@
 
     debug($unTableauAssociatif);
 
-    echo'<h2> Les variables globales </h2>';
+    echo '<h2> Les variables globales </h2>';
 
-    debug($GLOBALS)
+    // debug($_SERVER);
+
+    debug($GLOBALS['prenom']);
+
+    echo $_SERVER['REQUEST_TIME']+1235;
+
+    echo '<h2> Les constantes et les constantes magiques </h2>';
+
+    // Une constante permet de sauvegarder une valeur fixe
+    // Utile pour conserver les parametres de la base de données
+    // Par convention une constante se declare toujours en majuscule
+    define('CAPITAL_PAYS','Paris');
+
+    echo CAPITAL_PAYS;
+    const PAYS = 'Algerie';
+    echo '<br>';
+    echo PAYS;
+    echo '<br>';
+
+    // Afficher la version de PHP
+
+    echo PHP_VERSION;
+    echo '<br>';
+
+    // Afficher la valeur maximale d'un entier
+    echo PHP_INT_MAX;
+    echo '<br>';
+
+    // Afficher la valeur minimale d'un entier
+    echo PHP_INT_MIN;
+    echo '<br>';
+
+    // Afficher la ligne sur laquelle on est
+    echo __LINE__;
+    echo '<br>';
+
+    // Afficher le chemin du fichier
+    echo __FILE__;
+    echo '<br>';
+
+    // Afficher le dossier courant
+    echo __DIR__;
+
+    echo '<h2> Les tableaux associatifs </h2>';
+
+    // declarer 3 variables bleu,blanc,rouge
+
+    $bleu = 'bleu';
+    $blanc = 'blanc';
+    $rouge = 'rouge';
+
+    // Afficher bleu,blanc,rouge
+    echo "$bleu-$blanc-$rouge";
+    echo '<br>';
+    echo $bleu . '-' . $blanc . '-' . $rouge;
+    echo '<br>';
+
+    // Déclarer un tableau associatif :
+        /*
+            prenom: Kenza
+            nom : Mroudjae
+            age: 23
+            telephone : 06.58.86.94.65
+        */
+
+    $tableauPersonne = ['prenom' => 'Kenza', 'nom' => 'Mroudjae', 'age' => 23, 'telephone' => '06.58.86.94.65'];
+
+    // Afficher Bonjour je m'appelle Kenza Mroudjae, j'ai 41 ans et mon numéro de telephone est le ...
+    echo "Bonjour, je m'appelle" . ' ' . $tableauPersonne['prenom'] . ' ' . $tableauPersonne['nom'] .',' . " j'ai" . ' '. $tableauPersonne['age'] . ' ans et mon numéro de téléphone est le ' . $tableauPersonne['telephone'];
+
+    echo '<br>';
+
+    echo "Bonjour, je m'appelle {$tableauPersonne['prenom']} {$tableauPersonne['nom']}, j'ai {$tableauPersonne['age']} ans et mon numéro de téléphone est le {$tableauPersonne['telephone']}"
     ?>
 
 </body>
